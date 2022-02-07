ff490afd705fa3b81bb86788f1645adabc8c232f
Sourav Singh <souravsingh2609@gmail.com>
2022-02-07 13:44:04 +0530

    ArrayList modified

--- Test_Drive/src/Dynamic_Arrays/Arraylist.java
+++ Test_Drive/src/Dynamic_Arrays/Arraylist.java
@@ -14,7 +14,8 @@ public class Arraylist
 		arr.add(12); // adds 12 to the list
 		arr.add(1, 14); // adds 14 to the index 1
 		arr.add(15);
-		System.out.print(arr);
+		System.out.println(arr); // prints the list
+		System.out.println(arr.size()); // checks for the size of the arr
 		//
 		//
 		//
@@ -34,7 +35,14 @@ public class Arraylist
 		}
 		System.out.println(array.lastIndexOf(4)); // checks for the last index of 4 in array
 		
-		System.out.println(array.toArray()); // returns the list in array in correct order
+		 Integer[] n = new Integer[arr.size()]; 
+		 
+		 n = arr.toArray(n);// returns the list in array in correct order
+		 for(int i= 0; i < arr.size(); i++)
+		 {
+			 System.out.print(n[i]+" ");
+		 }
+		 System.out.println("");
 		//
 		//
 		//
@@ -43,6 +51,43 @@ public class Arraylist
 		
 		if(arr.isEmpty()) // checks if the array is Empty.
 			arr.add(3);
+		
+		if(arr.contains(3)) // checks if the array contains this element
+			System.out.println("Arr contains 3");
+		else
+			System.out.println("Array doesn't contain 3");
+			
+		if(array.containsAll(arr)) //checks if the List array contains all the elements present in List arr
+			System.out.println("Array contains all the elements in arr");
+		else
+			System.out.println("Array doesn't contain all the elements in arr");
+		
+		arr.add(2);
+		arr.add(5);
+		arr.add(7);
+		
+		if(arr.equals(array)) // equals() is used to compare two lists whether both lists have the same size and all corresponding pairs of elements in the two lists are equal or not
+			System.out.println("Equal");
+		else
+			System.out.println("Not equal");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		System.out.println(arr);
 		sc.close();
@@ -53,7 +98,9 @@ public class Arraylist
 /* 
 	Output
 	
-	[12, 14, 15]12
+	[12, 14, 15]
+	3
+	12
 	14
 	15
 	4
@@ -62,7 +109,10 @@ public class Arraylist
 	1
 	4
 	7
-	[Ljava.lang.Object;@55f96302
-	[3]
+	12 14 15 
+	Arr contains 3
+	Array contains all the elements in arr
+	Not equal
+	[3, 2, 5, 7]
 	
 */
